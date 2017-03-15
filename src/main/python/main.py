2ae01f0051766dca2bd08f025fe737057c9ee97e
Janne Hyttinen <hyttijan@cs.helsinki.fi>
2017-03-16 00:06:31 +0200

    PyBuilder lisätty

---
+++ src/main/python/main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Toimii")
+
+if __name__ == "__main__":
+    main()
---
+++ src/unittest/python/main_tests.py
@@ -0,0 +1,9 @@
+
+import unittest
+from main import main
+
+class MainTest(unittest.TestCase):
+    def test_should_issue_main(self):
+        print("wow")
+if __name__ == '__main__':
+    unittest.main()
